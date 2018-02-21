de1444ed7abc8c7009980eb221f9d0d7a29cd009
Olaniyi Oluwaseun <seunnnz@gmail.com>
2018-02-21 18:17:38 +0100

    Update heading.php

--- heading.php
+++ heading.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html">
-	<title>OAU Post Graduate | student login and Organisation search area</title>
+	<title>OAU Post Graduate | student login and Organisation</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/products.css">
